0d43295fc661412dc05fb1c04b61813847e5e725
MDMUHSINn <149122179+MDMUHSINn@users.noreply.github.com>
2026-01-21 16:26:53 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   language: "en",
-  ownerID: ["1444334428003504223", ""], 
-  mongodbUri : "mongodb+srv://shiva:shiva@discordbot.opd5w.mongodb.net/?retryWrites=true&w=majority",
+  ownerID: ["921743618316836875", ""], 
+  mongodbUri : "mongodb+srv://spella:sonic@spella.xmdijfx.mongodb.net/?appName=Spella",
   spotifyClientId : "d92baed9605a45a39ed7c2a2d960b1c1",
   spotifyClientSecret : "e9b29f6739de4315bc03b6d8a8e93b03",
   setupFilePath: './commands/setup.json',
@@ -11,7 +11,7 @@ module.exports = {
   embedColor: "#1db954",
   activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/QHzdUj2VqN",
   embedTimeout: 5,
   showProgressBar: false,  // Show progress bar in track embed
   showVisualizer: false,  // Show visualizer on music card (disabled for low-memory optimization)
